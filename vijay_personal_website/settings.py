0f81a215bb4bdf4c487ef07a0e0685f6761757e4
Vijay gwala <55628967+vijaygwala@users.noreply.github.com>
2020-10-17 23:40:53 +0530

    staged

--- vijay_personal_website/settings.py
+++ vijay_personal_website/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -121,7 +122,7 @@ AUTH_USER_MODEL = 'portfolio.CustomUser'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT=os.path.join(BASE_DIR, "staticfiles")
 STATICFILES_DIRS=[os.path.join(BASE_DIR, "static"),]
 
 STATIC_URL = '/static/'
